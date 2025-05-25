734bba8d715fc1cc0cb36066d7da0b96c6f09a73
Just_Choda <02220175.cst@rub.edu.bt>
2025-05-26 00:23:01 +0530

    depo

--- src/app/forgot-password/layout.tsx
+++ src/app/forgot-password/layout.tsx
@@ -1,1 +1,6 @@
- 
+  // src/app/forgot-password/layout.tsx
+  import React from "react";
+
+  export default function ForgotPasswordLayout({ children }: { children: React.ReactNode }) {
+    return <>{children}</>;
+  }
